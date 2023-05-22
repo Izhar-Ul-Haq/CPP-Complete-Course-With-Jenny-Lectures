268586ee901c059bc96824f065a1154d7578e32a
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-22 10:27:05 +0500

    File Handling

--- fileHandling.cpp
+++ fileHandling.cpp
@@ -3,15 +3,14 @@ using namespace std;
 int main()
 {
 	cout << "Izhar Ul Haq" << endl;
-	
-	
-	
-	
-	
-	
+	string data;
+	cout << "Enter data in String" << endl;
+	cin >> data;
+	cout << "You entered: " << data <<endl;
+	cout << "Enter another one:" << endl;
+	cin >> data;
+	cout << "You entered:" << data << endl;
+	// The cin will only read the data before the spaces
 	return 0;
-	
- 
- 
 }
 
