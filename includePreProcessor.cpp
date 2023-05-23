89e7ca46f91d0ee5e022250f6b86c10238118fb4
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-23 14:55:08 +0500

    Including PreProcessor in CPP

---
+++ includePreProcessor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include "personClass.cpp"
+
+int main()
+{
+    cout << "Izhar Ul Haq" <<endl;
+    Person izhar("Izhar Ul Haq", 26);
+    izhar.display();
+    return 0;
+}
+
