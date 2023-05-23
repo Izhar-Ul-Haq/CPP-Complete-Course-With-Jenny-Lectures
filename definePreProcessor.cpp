c2de66dc8f8573840a5cdfa21c9e1bcb0e872d14
Izhar-Ul-Haq <iizharulhaq9@gmail.com>
2023-05-23 13:00:20 +0500

    Pre Processor in CPP

---
+++ definePreProcessor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#define COUNTRY "Pakistan"
+#define NAME "Izhar Ul Haq"
+#define ONE 1
+#define TWO ONE+ONE
+#define THREE TWO+ONE
+using namespace std;
+int main()
+{
+    cout << "Izhar Ul Haq" <<endl;
+    cout << "I live in Country: " << COUNTRY << endl;
+    cout << "My name is: " << NAME << endl;
+    cout << "THREE: " << THREE <<endl;
+	return 0;
+}
+
